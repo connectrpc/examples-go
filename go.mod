e3f40d14eff9cb63d90cfc1f04d44adacfa56494
Alan Chiu <awlchiiu@gmail.com>
2022-05-23 11:42:23 -0700

    Add handler wire up in main

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.18
 require (
 	github.com/bufbuild/connect-go v0.0.0-20220520175512-2b3d3442ffb8
 	github.com/stretchr/testify v1.7.1
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2
 	google.golang.org/protobuf v1.28.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- main.go
+++ main.go
@@ -15,8 +15,63 @@
 package main
 
 import (
-	// Temporary: keeps connect in go.mod so we can install protoc-gen-connect-go.
-	_ "github.com/bufbuild/connect-go"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/bufbuild/connect-demo/internal/gen/connect-go/buf/connect/demo/eliza/v1/elizav1connect"
+	elizav1 "github.com/bufbuild/connect-demo/internal/gen/go/buf/connect/demo/eliza/v1"
+	connect "github.com/bufbuild/connect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
-func main() {}
+// ElizaServer implements some trivial business logic. The Protobuf
+// definition for this API is in proto/buf/connect/demo/eliza/v1/eliza.proto.
+type ElizaServer struct {
+	elizav1connect.ElizaServiceHandler
+}
+
+// Say is a unary request demo. This method should allow for a one sentence
+// response given a one sentence request.
+func (e *ElizaServer) Say(ctx context.Context, req *connect.Request[elizav1.SayRequest]) (*connect.Response[elizav1.SayResponse], error) {
+	log.Println("Saying....")
+	log.Println("Request headers: ", req.Header())
+
+	res := connect.NewResponse(&elizav1.SayResponse{
+		Sentence: "hello",
+	})
+	return res, nil
+}
+
+// Converse is a bi-directional request demo. This method should allow for
+// many requests and many responses.
+func (e *ElizaServer) Converse(context.Context, *connect.BidiStream[elizav1.ConverseRequest, elizav1.ConverseResponse]) error {
+	log.Println("Conversing....")
+	return fmt.Errorf("")
+}
+
+func main() {
+	log.Println("Starting....")
+	// The business logic here is trivial, but the rest of the example is meant
+	// to be somewhat realistic. This server has basic timeouts configured, and
+	// it also exposes gRPC's server reflection and health check APIs.
+
+	// protoc-gen-connect-go generates constructors that return plain net/http
+	// Handlers, so they're compatible with most Go HTTP routers and middleware
+	// (for example, net/http's StripPrefix). Each handler automatically supports
+	// the Connect, gRPC, and gRPC-Web protocols.
+	mux := http.NewServeMux()
+	elizaServiceHandler := &ElizaServer{} // our business logic
+	path, handler := elizav1connect.NewElizaServiceHandler(elizaServiceHandler)
+	mux.Handle(path, handler)
+	err := http.ListenAndServe(
+		":9000",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
