73442b82483b92f712351b757990d736c2673b01
Alan Chiu <awlchiu+gh@gmail.com>
2022-05-23 21:13:30 -0700

    Implement Protobuf service (#4)
    Implement `ElizaService`, with health and reflection support.

--- main.go
+++ main.go
@@ -15,8 +15,103 @@
 package main
 
 import (
-	// Temporary: keeps connect in go.mod so we can install protoc-gen-connect-go.
-	_ "github.com/bufbuild/connect-go"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/bufbuild/connect-demo/internal/eliza"
+	"github.com/bufbuild/connect-demo/internal/gen/connect-go/buf/connect/demo/eliza/v1/elizav1connect"
+	elizav1 "github.com/bufbuild/connect-demo/internal/gen/go/buf/connect/demo/eliza/v1"
+	connect "github.com/bufbuild/connect-go"
+	grpchealth "github.com/bufbuild/connect-grpchealth-go"
+	grpcreflect "github.com/bufbuild/connect-grpcreflect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
-func main() {}
+type elizaServer struct{}
+
+func (e *elizaServer) Say(
+	ctx context.Context,
+	req *connect.Request[elizav1.SayRequest],
+) (*connect.Response[elizav1.SayResponse], error) {
+	reply, _ := eliza.Reply(req.Msg.Sentence) // ignore end-of-conversation detection
+	return connect.NewResponse(&elizav1.SayResponse{
+		Sentence: reply,
+	}), nil
+}
+
+func (e *elizaServer) Converse(
+	ctx context.Context,
+	stream *connect.BidiStream[elizav1.ConverseRequest, elizav1.ConverseResponse],
+) error {
+	for {
+		if err := ctx.Err(); err != nil {
+			return err
+		}
+		request, err := stream.Receive()
+		if err != nil && errors.Is(err, io.EOF) {
+			return nil
+		} else if err != nil {
+			return fmt.Errorf("receive request: %w", err)
+		}
+		reply, endSession := eliza.Reply(request.Sentence)
+		if err := stream.Send(&elizav1.ConverseResponse{Sentence: reply}); err != nil {
+			return fmt.Errorf("send response: %w", err)
+		}
+		if endSession {
+			return nil
+		}
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	compress1KB := connect.WithCompressMinBytes(1024)
+	mux.Handle(elizav1connect.NewElizaServiceHandler(
+		&elizaServer{},
+		compress1KB,
+	))
+	mux.Handle(grpchealth.NewHandler(
+		grpchealth.NewStaticChecker(elizav1connect.ElizaServiceName),
+		compress1KB,
+	))
+	mux.Handle(grpcreflect.NewHandlerV1(
+		grpcreflect.NewStaticReflector(elizav1connect.ElizaServiceName),
+		compress1KB,
+	))
+
+	addr := "localhost:8080"
+	if port := os.Getenv("PORT"); port != "" {
+		addr = ":" + port
+	}
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           h2c.NewHandler(mux, &http2.Server{}),
+		ReadHeaderTimeout: time.Second,
+		ReadTimeout:       60 * time.Second,
+		WriteTimeout:      60 * time.Second,
+		MaxHeaderBytes:    8 * 1024, // 8KiB
+	}
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP listen and serve: %v", err)
+		}
+	}()
+
+	<-signals
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("HTTP shutdown: %v", err) // nolint:gocritic
+	}
+}
