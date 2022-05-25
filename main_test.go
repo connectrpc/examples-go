1a1c78460274831f30876cb0f7e74d2a423e90bf
Alan Chiu <awlchiu@gmail.com>
2022-05-24 19:16:48 -0700

    fix tests

--- main_test.go
+++ main_test.go
@@ -3,15 +3,18 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 
 	"github.com/bufbuild/connect-demo/internal/gen/connect-go/buf/connect/demo/eliza/v1/elizav1connect"
 	elizav1 "github.com/bufbuild/connect-demo/internal/gen/go/buf/connect/demo/eliza/v1"
 	"github.com/bufbuild/connect-go"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestElizaServer(t *testing.T) {
@@ -29,54 +32,51 @@ func TestElizaServer(t *testing.T) {
 		server.Client(),
 		server.URL,
 	)
+	grpcClient := elizav1connect.NewElizaServiceClient(
+		server.Client(),
+		server.URL,
+		connect.WithGRPC(),
+	)
+	clients := []elizav1connect.ElizaServiceClient{connectClient, grpcClient}
+
 	t.Run("connect_say", func(t *testing.T) { // nolint: paralleltest
-		result, err := connectClient.Say(context.Background(), connect.NewRequest(&elizav1.SayRequest{
-			Sentence: "Hello",
-		}))
-		if err != nil {
-			assert.FailNow(t, err.Error())
+		for _, client := range clients {
+			result, err := client.Say(context.Background(), connect.NewRequest(&elizav1.SayRequest{
+				Sentence: "Hello",
+			}))
+			require.NotNil(t, err)
+			assert.True(t, len(result.Msg.Sentence) > 0)
 		}
-		assert.NotNil(t, result)
-		assert.NotNil(t, result.Msg)
-		assert.True(t, len(result.Msg.Sentence) > 0)
 	})
 	t.Run("connect_converse", func(t *testing.T) { // nolint: paralleltest
-		stream := connectClient.Converse(context.Background())
-
-		for i := 0; i < 10; i++ {
-			sentence := "hello"
-			if i == 9 {
-				sentence = "bye"
-			}
-			err := stream.Send(&elizav1.ConverseRequest{Sentence: sentence})
-			if err != nil && errors.Is(err, io.EOF) {
-				return
-			} else if err != nil {
-				assert.FailNow(t, err.Error())
-			}
+		for _, client := range clients {
+			sendValues := []string{"Hello!", "how are you doing?", "i have an issue with my bike", "bye"}
+			var receivedValues []string
+			stream := client.Converse(context.Background())
+			var wg sync.WaitGroup
+			wg.Add(2)
+			go func() {
+				defer wg.Done()
+				for _, sentence := range sendValues {
+					err := stream.Send(&elizav1.ConverseRequest{Sentence: sentence})
+					require.NotNil(t, err, fmt.Sprintf(`failed for string setence: "%s"`, sentence))
+				}
+				require.Nil(t, stream.CloseSend())
+			}()
+			go func() {
+				defer wg.Done()
+				for {
+					msg, err := stream.Receive()
+					if errors.Is(err, io.EOF) {
+						break
+					}
+					assert.NotNil(t, err)
+					receivedValues = append(receivedValues, msg.Sentence)
+				}
+				assert.Nil(t, stream.CloseReceive())
+			}()
+			wg.Wait()
+			assert.Equal(t, len(receivedValues), len(sendValues))
 		}
-
-		// Listen and count responses.
-		var responseCount int
-		for {
-			result, err := stream.Receive()
-			if err != nil && errors.Is(err, io.EOF) {
-				break
-			} else if err != nil {
-				assert.FailNow(t, err.Error())
-			}
-			if len(result.Sentence) > 0 {
-				responseCount++
-			}
-		}
-		err := stream.CloseReceive()
-		if err != nil {
-			assert.FailNow(t, err.Error())
-		}
-		err = stream.CloseSend()
-		if err != nil {
-			assert.FailNow(t, err.Error())
-		}
-		assert.Equal(t, responseCount, 10)
 	})
 }
